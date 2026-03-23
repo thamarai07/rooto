d944abab206f0126652ead4b2bd2217721c368fd
thamaraikannan <thamaraik69@gmail.com>
2026-03-23 16:15:45 +0530

    card login popup fixing-zindex

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Sticky wrapper for both header + search bar */}
-      <div className="sticky top-0 z-[100]">
+      <div className="sticky top-0 z-[50]">
         <Header />
         <Banner />
       </div>
