626031886498c60d4515951e30efdeae0652e57b
thamarai kannan <thamaraik69@gmail.com>
2026-08-16 13:53:54 +0530

    fix: reset build script to next build

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,6 +2,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-	buildCommand: "npm run build:next",
+	// For best results consider enabling R2 caching
+	// See https://opennext.js.org/cloudflare/caching for more details
 });
+
 
